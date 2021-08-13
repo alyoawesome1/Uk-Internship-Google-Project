47870c5d7f079fb7a30f8a024f270278fb0fa421
alyoawesome1 <88876846+alyoawesome1@users.noreply.github.com>
2021-08-13 00:15:44 -0700

    Update SortByTitle.java

--- google-code-sample-main/java/src/main/java/com/google/SortByTitle.java
+++ google-code-sample-main/java/src/main/java/com/google/SortByTitle.java
@@ -2,8 +2,7 @@ package com.google;
 
 import java.util.Comparator;
 
-// This class can be used by a Collection of Videos like a List of Videos. By using this,
-// I can sort a list of Videos by their title easily whenever we need to display videos in a sorted way.
+
 public class SortByTitle implements Comparator<Video> {
     @Override
     public int compare(Video o1, Video o2) {
